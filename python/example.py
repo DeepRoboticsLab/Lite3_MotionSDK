a00c39812fa35f4a90476de16455f0e9689af0da
mazunwang <mazunwang@163.com>
2024-10-23 18:51:51 +0800

    add-python version confirmed

--- python/example.py
+++ python/example.py
@@ -1,53 +1,68 @@
 # -*- coding: UTF-8 -*-
 import sys
+import time
 sys.path.append('./lib')
 import deeprobotics_lite3_motion_sdk_py as dr
 from motion_example import MotionExample
 
 
-is_message_updated_ = False
 
+is_message_updated = False
 def handler(code):
-    if(code == 0x0906):
-      is_message_updated_ = True
-    
+  global is_message_updated
+  if(code == 0x0906):
+    is_message_updated = True
+      
+def print_robot_data(robot_data):
+  print("joint_pos: ")
+  for i in range(12):
+    print(round(robot_data.joint_data.joint_data[i].position, 3), end=" ")
+  print("\nimu_acc: ")
+  print(round(robot_data.imu.acc_x, 3),\
+        round(robot_data.imu.acc_y, 3),\
+        round(robot_data.imu.acc_z, 3))
 
-if __name__ == "__main__" :
-  timer = dr.DRTimer()
-  now_time = 0.0
-  start_time = 0.0
+timer = dr.DRTimer()
 
-  sender = dr.Sender('192.168.1.120', 43893)
-  receiver = dr.Receiver(43897)
-  receiver.RegisterCallBack(handler)
-  robot_data = receiver.GetState()
-  receiver.StartWork()
-  timer.TimeInit(1)
-  sender.RobotStateInit()
-  
+sender = dr.Sender('192.168.1.120', 43893)
+receiver = dr.Receiver(43897)
+receiver.RegisterCallBack(handler)
+robot_data = receiver.GetState()
+receiver.StartWork()
+timer.TimeInit(1)
+sender.RobotStateInit()
+robot_set_up_demo = MotionExample()
+time_tick = 0
+robot_joint_cmd = dr.RobotCmd()
+
+while not is_message_updated or time_tick < 1000 :#make sure received data is correct
   start_time = timer.GetCurrentTime()
+  robot_data = receiver.GetState()
+  robot_set_up_demo.GetInitData(robot_data.joint_data, 0) 
+  time_tick += 1
+  time.sleep(0.001)  
+  if time_tick%1000==0:
+    print_robot_data(robot_data)
   
-  robot_set_up_demo = MotionExample()
-  robot_set_up_demo.GetInitData(receiver.GetState().joint_data, 0.000)
   
-  time_tick = 0
-  robot_joint_cmd = dr.RobotCmd()
-  
-  while True:
-    if timer.TimerInterrupt() == True:
-      continue
-    now_time = timer.GetIntervalTime(start_time)
-    time_tick = time_tick+1
+time_tick = 0
+while is_message_updated:
+  now_time = timer.GetIntervalTime(start_time)
+  robot_data = receiver.GetState()
+  if timer.TimerInterrupt() == True:
+    continue
 
-    if time_tick < 1000:
-      robot_set_up_demo.PreStandUp(robot_joint_cmd, now_time, receiver.GetState())
-    if time_tick == 1000:
-      robot_set_up_demo.GetInitData(receiver.GetState().joint_data, now_time)
-    if time_tick >= 1000:
-      robot_set_up_demo.StandUp(robot_joint_cmd, now_time, receiver.GetState())
-    if time_tick >= 10000:
-      sender.ControlGet(1)
-      break
-    if is_message_updated_:
-      pass
-    sender.SendCmd(robot_joint_cmd)
+  time_tick = time_tick+1
+  if time_tick < 2000:
+    robot_set_up_demo.PreStandUp(robot_joint_cmd, now_time, robot_data)
+  if time_tick == 2000:
+    robot_set_up_demo.GetInitData(robot_data.joint_data, now_time)
+  if time_tick >= 2000:
+    robot_set_up_demo.StandUp(robot_joint_cmd, now_time, robot_data)
+  if time_tick >= 10000:
+    sender.ControlGet(1)
+    break
+  if is_message_updated == True:
+    print_robot_data(robot_data)
+    # sender.SendCmd(robot_joint_cmd)
+    continue
--- python/motion_example.py
+++ python/motion_example.py
@@ -5,64 +5,39 @@ sys.path.append('./lib')
 import numpy
 import deeprobotics_lite3_motion_sdk_py as dr
 
-
-
 kDegree2Radian = 3.1415926 / 180
-
-goal_angle_fl = numpy.zeros(3) 
-goal_angle_hl = numpy.zeros(3) 
-goal_angle_fr = numpy.zeros(3)
-goal_angle_hr = numpy.zeros(3)
-init_angle_fl = numpy.zeros(3)
-init_angle_fr = numpy.zeros(3)
-init_angle_hl = numpy.zeros(3)
-init_angle_hr = numpy.zeros(3)
 
 class MotionExample:
   def __init__(self):
     self.init_time = 0.0
+    self.init_angle_fl = numpy.zeros(3)
+    self.init_angle_fr = numpy.zeros(3)
+    self.init_angle_hl = numpy.zeros(3)
+    self.init_angle_hr = numpy.zeros(3)
   
-  def CubicSpline(self, 
-                  init_position, init_velocity,
-                  goal_position, goal_velocity,
-                  run_time, cycle_time, total_time):
-    a=0.0
-    b=0.0
-    c = 0.0
-    d = 0.0    
-    d = init_position
-    c = init_velocity
-    a = (goal_velocity * total_time - 2 * goal_position + init_velocity * total_time + 
-        2 * init_position) /pow(total_time, 3)
-    b = (3 * goal_position - goal_velocity * total_time - 2 * init_velocity * total_time - 
-        3 * init_position) / pow(total_time, 2)
+  def CubicSpline(self, x0, v0, xf, vf, run_time, cycle_time, total_time):
+    d = x0
+    c = v0
+    a = (vf * total_time - 2 * xf + v0 * total_time + 
+        2 * x0) /pow(total_time, 3)
+    b = (3 * xf - vf * total_time - 2 * v0 * total_time - 
+        3 * x0) / pow(total_time, 2)
 
     if run_time > total_time:
       run_time = total_time
-    sub_goal_position = a * pow(run_time, 3) + b * pow(run_time, 2) + c * run_time + d
+    sub_xf = a * pow(run_time, 3) + b * pow(run_time, 2) + c * run_time + d
 
     if run_time + cycle_time > total_time:
-      run_time = total_time - cycle_time
-    sub_goal_position_next = a * pow(run_time + cycle_time, 3) + \
+      run_time = total_time
+    sub_xf_next = a * pow(run_time + cycle_time, 3) + \
                              b * pow(run_time + cycle_time, 2) + \
                              c * (run_time + cycle_time) + d
-
-    if run_time + cycle_time * 2 > total_time:
-      run_time = total_time - cycle_time * 2
-    sub_goal_position_next2 = a * pow(run_time + cycle_time * 2, 3) + \
-                              b * pow(run_time + cycle_time * 2, 2) + \
-                              c * (run_time + cycle_time * 2) + d
-    return sub_goal_position, sub_goal_position_next, sub_goal_position_next2
+    return sub_xf, sub_xf_next
   
-  def SwingToAngle(self,
-                   initial_angle, final_angle,
-                   total_time, run_time,
-                   cycle_time, side,
-                   cmd, data):
+  def SwingToAngle(self, q0, qf, total_time, run_time, cycle_time, side, cmd, data):
     goal_angle = numpy.zeros(3)
     goal_angle_next = numpy.zeros(3)
-    goal_angle_next2 = numpy.zeros(3)
-    goal_velocity = numpy.zeros(3)
+    goal_vel = numpy.zeros(3)
     leg_side = 0
 
     if side == "FL":
@@ -75,52 +50,32 @@ class MotionExample:
       leg_side = 3
     else:
       print("Leg Side Error!!!")
-
-    final_angle = final_angle
     
     for j in range(0, 3):
-      goal_angle[j], goal_angle_next[j], goal_angle_next2[j] = \
-        self.CubicSpline(initial_angle[j], 0, final_angle[j], 0, run_time,
+      goal_angle[j], goal_angle_next[j] = \
+        self.CubicSpline(q0[j], 0, qf[j], 0, run_time,
                          cycle_time, total_time)
-      goal_velocity[j] = (goal_angle_next[j] - goal_angle[j]) / cycle_time
+      goal_vel[j] = (goal_angle_next[j] - goal_angle[j]) / cycle_time
 
-    if True:
-      cmd.joint_cmd[3 * leg_side].kp = 60
-      cmd.joint_cmd[3 * leg_side + 1].kp = 60
-      cmd.joint_cmd[3 * leg_side + 2].kp = 60
-      cmd.joint_cmd[3 * leg_side].kd = 0.7
-      cmd.joint_cmd[3 * leg_side + 1].kd = 0.7
-      cmd.joint_cmd[3 * leg_side + 2].kd = 0.7
-      cmd.joint_cmd[3 * leg_side].position = goal_angle[0]
-      cmd.joint_cmd[3 * leg_side + 1].position = goal_angle[1]
-      cmd.joint_cmd[3 * leg_side + 2].position = goal_angle[2]
-      cmd.joint_cmd[3 * leg_side].velocity = goal_velocity[0]
-      cmd.joint_cmd[3 * leg_side + 1].velocity = goal_velocity[1]
-      cmd.joint_cmd[3 * leg_side + 2].velocity = goal_velocity[2]
-      for i in range(0, 12):
-        cmd.joint_cmd[i].torque = 0
-    else:
-      cmd.joint_cmd[3 * leg_side].kp = 0
-      cmd.joint_cmd[3 * leg_side + 1].kp = 0
-      cmd.joint_cmd[3 * leg_side + 2].kp = 0
-      cmd.joint_cmd[3 * leg_side].kd = 0
-      cmd.joint_cmd[3 * leg_side + 1].kd = 0
-      cmd.joint_cmd[3 * leg_side + 2].kd = 0
-      cmd.joint_cmd[3 * leg_side].position = 0
-      cmd.joint_cmd[3 * leg_side + 1].position = 0
-      cmd.joint_cmd[3 * leg_side + 2].position = 0
-      cmd.joint_cmd[3 * leg_side].velocity = 0
-      cmd.joint_cmd[3 * leg_side + 1].velocity = 0
-      cmd.joint_cmd[3 * leg_side + 2].velocity = 0
-      cmd.joint_cmd[3* leg_side].torque = 60 * (goal_angle[0] - data.joint_data.joint_data[3* leg_side].position) \
-                                      + 0.7 * (goal_velocity[0] - data.joint_data.joint_data[3* leg_side].velocity)
-      cmd.joint_cmd[3* leg_side+1].torque = 80 * (goal_angle[1] - data.joint_data.joint_data[3* leg_side+1].position) \
-                                      + 0.7 * (goal_velocity[1] - data.joint_data.joint_data[3* leg_side+1].velocity)
-      cmd.joint_cmd[3* leg_side+2].torque = 80 * (goal_angle[2] - data.joint_data.joint_data[3* leg_side+2].position) \
-                                      + 0.7 * (goal_velocity[2] - data.joint_data.joint_data[3* leg_side+2].velocity)
+    cmd.joint_cmd[3 * leg_side].kp = 60
+    cmd.joint_cmd[3 * leg_side + 1].kp = 60
+    cmd.joint_cmd[3 * leg_side + 2].kp = 60
+    cmd.joint_cmd[3 * leg_side].kd = 0.7
+    cmd.joint_cmd[3 * leg_side + 1].kd = 0.7
+    cmd.joint_cmd[3 * leg_side + 2].kd = 0.7
+    cmd.joint_cmd[3 * leg_side].position = goal_angle[0]
+    cmd.joint_cmd[3 * leg_side + 1].position = goal_angle[1]
+    cmd.joint_cmd[3 * leg_side + 2].position = goal_angle[2]
+    cmd.joint_cmd[3 * leg_side].velocity = goal_vel[0]
+    cmd.joint_cmd[3 * leg_side + 1].velocity = goal_vel[1]
+    cmd.joint_cmd[3 * leg_side + 2].velocity = goal_vel[2]
+    for i in range(0, 12):
+      cmd.joint_cmd[i].torque = 0
+      
+
     
   def PreStandUp(self, cmd, time, data_state):
-    standup_time = 1.0
+    standup_time = 2.0
     cycle_time = 0.001
     goal_angle_fl = [0 * kDegree2Radian, -70 * kDegree2Radian, 150 * kDegree2Radian]
     goal_angle_fr = [0 * kDegree2Radian, -70 * kDegree2Radian, 150 * kDegree2Radian]
@@ -128,17 +83,17 @@ class MotionExample:
     goal_angle_hr = [0 * kDegree2Radian, -70 * kDegree2Radian, 150 * kDegree2Radian]
 
     if time <= self.init_time + standup_time:
-      self.SwingToAngle(init_angle_fl, goal_angle_fl, standup_time, time - self.init_time,
+      self.SwingToAngle(self.init_angle_fl, goal_angle_fl, standup_time, time - self.init_time,
                         cycle_time, "FL", cmd, data_state)
-      self.SwingToAngle(init_angle_fr, goal_angle_fr, standup_time, time - self.init_time,
+      self.SwingToAngle(self.init_angle_fr, goal_angle_fr, standup_time, time - self.init_time,
                         cycle_time, "FR", cmd, data_state)
-      self.SwingToAngle(init_angle_hl, goal_angle_hl, standup_time, time - self.init_time,
+      self.SwingToAngle(self.init_angle_hl, goal_angle_hl, standup_time, time - self.init_time,
                         cycle_time, "HL", cmd, data_state)
-      self.SwingToAngle(init_angle_hr, goal_angle_hr, standup_time, time - self.init_time,
+      self.SwingToAngle(self.init_angle_hr, goal_angle_hr, standup_time, time - self.init_time,
                         cycle_time, "HR", cmd, data_state)
    
   def StandUp(self, cmd, time, data_state):
-    standup_time = 1.5
+    standup_time = 2.5
     cycle_time = 0.001
     goal_angle_fl = [0 * kDegree2Radian, -42 * kDegree2Radian, 78 * kDegree2Radian] 
     goal_angle_fr = [0 * kDegree2Radian, -42 * kDegree2Radian, 78 * kDegree2Radian]
@@ -146,13 +101,13 @@ class MotionExample:
     goal_angle_hr = [0 * kDegree2Radian, -42 * kDegree2Radian, 78 * kDegree2Radian]
 
     if (time <= self.init_time + standup_time):
-      self.SwingToAngle(init_angle_fl, goal_angle_fl, standup_time, time - self.init_time,
+      self.SwingToAngle(self.init_angle_fl, goal_angle_fl, standup_time, time - self.init_time,
                         cycle_time, "FL", cmd, data_state)
-      self.SwingToAngle(init_angle_fr, goal_angle_fr, standup_time, time - self.init_time,
+      self.SwingToAngle(self.init_angle_fr, goal_angle_fr, standup_time, time - self.init_time,
                         cycle_time, "FR", cmd, data_state)
-      self.SwingToAngle(init_angle_hl, goal_angle_hl, standup_time, time - self.init_time,
+      self.SwingToAngle(self.init_angle_hl, goal_angle_hl, standup_time, time - self.init_time,
                         cycle_time, "HL", cmd, data_state)
-      self.SwingToAngle(init_angle_hr, goal_angle_hr, standup_time, time - self.init_time,
+      self.SwingToAngle(self.init_angle_hr, goal_angle_hr, standup_time, time - self.init_time,
                         cycle_time, "HR", cmd, data_state)
     else:
       for i in range(0, 12):
@@ -170,18 +125,18 @@ class MotionExample:
         
   def GetInitData(self, data, time):
     self.init_time = time
-    init_angle_fl[0] = data.joint_data[0].position
-    init_angle_fl[1] = data.joint_data[1].position
-    init_angle_fl[2] = data.joint_data[2].position
+    self.init_angle_fl[0] = data.joint_data[0].position
+    self.init_angle_fl[1] = data.joint_data[1].position
+    self.init_angle_fl[2] = data.joint_data[2].position
 
-    init_angle_fr[0] = data.joint_data[3].position
-    init_angle_fr[1] = data.joint_data[4].position
-    init_angle_fr[2] = data.joint_data[5].position
+    self.init_angle_fr[0] = data.joint_data[3].position
+    self.init_angle_fr[1] = data.joint_data[4].position
+    self.init_angle_fr[2] = data.joint_data[5].position
 
-    init_angle_hl[0] = data.joint_data[6].position
-    init_angle_hl[1] = data.joint_data[7].position
-    init_angle_hl[2] = data.joint_data[8].position
+    self.init_angle_hl[0] = data.joint_data[6].position
+    self.init_angle_hl[1] = data.joint_data[7].position
+    self.init_angle_hl[2] = data.joint_data[8].position
 
-    init_angle_hr[0] = data.joint_data[9].position
-    init_angle_hr[1] = data.joint_data[10].position
-    init_angle_hr[2] = data.joint_data[11].position
+    self.init_angle_hr[0] = data.joint_data[9].position
+    self.init_angle_hr[1] = data.joint_data[10].position
+    self.init_angle_hr[2] = data.joint_data[11].position
